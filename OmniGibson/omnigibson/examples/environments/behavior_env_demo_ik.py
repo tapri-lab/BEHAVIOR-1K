d465b32ef6f4b1450c60dd350293fc398d30759a
BEHAVIOR-1K <behavior-1k@ACTGPT-P16-U.dfki.uni-bremen.de>
2025-10-23 16:05:31 +0200

    update simulation demo with r1pro and operational space controller

--- OmniGibson/omnigibson/examples/environments/behavior_env_demo_ik.py
+++ OmniGibson/omnigibson/examples/environments/behavior_env_demo_ik.py
@@ -4,8 +4,9 @@ import yaml
 
 import omnigibson as og
 from omnigibson.macros import gm
-from omnigibson.utils.ui_utils import choose_from_options
 
+import omnigibson.lazy as lazy
+from omnigibson.utils.ui_utils import KeyboardRobotController, choose_from_options
 # Make sure object states are enabled
 gm.ENABLE_OBJECT_STATES = True
 gm.USE_GPU_DYNAMICS = False
@@ -20,21 +21,10 @@ def main(random_selection=False, headless=False, short_exec=False):
     """
     og.log.info(f"Demo {__file__}\n    " + "*" * 80 + "\n    Description:\n" + main.__doc__ + "*" * 80)
 
-    # Ask the user whether they want online object sampling or not
-    sampling_options = {
-        False: "Use a pre-sampled cached BEHAVIOR activity scene",
-        True: "Sample the BEHAVIOR activity in an online fashion",
-    }
-    should_sample = choose_from_options(
-        options=sampling_options, name="online object sampling", random_selection=random_selection
-    )
-
     # Load the pre-selected configuration and set the online_sampling flag
     config_filename = os.path.join(og.example_config_path, "r1pro_behavior_ik.yaml")
     cfg = yaml.load(open(config_filename, "r"), Loader=yaml.FullLoader)
-    cfg["task"]["online_object_sampling"] = should_sample
-    cfg["task"]["use_presampled_robot_pose"] = not should_sample
-
+    #
     # Load the environment
     env = og.Environment(configs=cfg)
 
@@ -44,19 +34,40 @@ def main(random_selection=False, headless=False, short_exec=False):
     )
 
     # Allow user to move camera more easily
-    og.sim.enable_viewer_camera_teleoperation()
+    #og.sim.enable_viewer_camera_teleoperation()
 
-    # Run a simple loop and reset periodically
-    max_iterations = 10 if not short_exec else 1
-    for j in range(max_iterations):
-        og.log.info("Resetting environment")
-        env.reset()
-        for i in range(100):
-            action = env.robots[0].action_space.sample()
-            state, reward, terminated, truncated, info = env.step(action * 0.1)
-            if terminated or truncated:
-                og.log.info("Episode finished after {} timesteps".format(i + 1))
-                break
+    robot = env.robots[0]
+    # Create teleop controller
+    action_generator = KeyboardRobotController(robot=robot)
+
+    # Register custom binding to reset the environment
+    action_generator.register_custom_keymapping(
+        key=lazy.carb.input.KeyboardInput.R,
+        description="Reset the robot",
+        callback_fn=lambda: env.reset(),
+    )
+
+    # Print out relevant keyboard info if using keyboard teleop
+    action_generator.print_keyboard_teleop_info()
+
+    max_steps = -1 if not short_exec else 100
+    step = 0
+
+    while step != max_steps:
+        action = action_generator.get_teleop_action()
+        env.step(action=action)
+        step += 1
+    # # Run a simple loop and reset periodically
+    # max_iterations = 10 if not short_exec else 1
+    # for j in range(max_iterations):
+    #     og.log.info("Resetting environment")
+    #     env.reset()
+    #     for i in range(100):
+    #         action = env.robots[0].action_space.sample()
+    #         state, reward, terminated, truncated, info = env.step(action * 0.1)
+    #         if terminated or truncated:
+    #             og.log.info("Episode finished after {} timesteps".format(i + 1))
+    #             break
 
     # Always close the environment at the end
     og.shutdown()
